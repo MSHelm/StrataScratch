df7dcdd73952e2a29001e193ade6f607403800a7
Martin Helm <54892009+MSHelm@users.noreply.github.com>
2022-06-16 14:23:33 +0200

    Create NumberOfViolations.sql

---
+++ NumberOfViolations.sql
@@ -0,0 +1,6 @@
+SELECT EXTRACT(YEAR from inspection_date) AS year, COUNT(*) AS violation_count
+FROM sf_restaurant_health_violations
+WHERE violation_id IS NOT NULL
+AND business_name = 'Roxanne Cafe'
+GROUP BY year
+ORDER BY violation_count
